479afd5200e4676bcc40a8775c24df800e5c48a1
Suraj31shah <suraj0631shah@gmail.com>
2025-01-16 12:05:03 +0530

    Update Ass-4.py

--- Ass-4.py
+++ Ass-4.py
@@ -1,11 +1,9 @@
 num1=input("Enter first number: ")
 num1=int(num1)
-num2=input("Enter first number: ")
+num2=input("Enter second number: ")
+num2=int(num2)
 print("Before Swapping")
 print("num1=",num1,"num2=",num2)
-num2=int(num2)
-num1=num1^num2
-num2=num1^num2
-num1=num1^num2
+num1,num2=num2,num1
 print("After Swapping")
 print("num1=",num1,"num2=",num2)
